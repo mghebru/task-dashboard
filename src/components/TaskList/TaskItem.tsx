0e3b12d116da0040e13fbf53465cc0e65123e7ce
mghebru <mghebru123@gmail.com>
2026-02-25 11:27:33 -0500

    Update TaskItem.tsx

--- src/components/TaskList/TaskItem.tsx
+++ src/components/TaskList/TaskItem.tsx
@@ -1,1 +1,33 @@
+import type { Task } from "../../types";
 
+interface TaskItemProps {
+  task: Task;
+  onDelete: (id: string) => void;
+  onToggle: (id: string) => void;
+}
+
+const TaskItem: React.FC<TaskItemProps> = ({
+  task,
+  onDelete,
+  onToggle,
+}) => {
+  return (
+    <li>
+      <span
+        style={{
+          textDecoration:
+            task.status === "completed" ? "line-through" : "none",
+        }}
+      >
+        {task.title}
+      </span>
+      <br></br>
+
+      <button onClick={() => onToggle(task.id)}>Complete</button>
+     <br></br>
+      <button onClick={() => onDelete(task.id)}>Delete</button>
+    </li>
+  );
+};
+
+export default TaskItem;
